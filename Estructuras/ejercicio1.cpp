10ff6ac0c30d4df2271a0ce40859ffdbcdf273fd
rafael yepez <xander21yl@gmail.com>
2024-11-20 09:41:02 -0500

    C++ 1

---
+++ Estructuras/ejercicio1.cpp
@@ -0,0 +1,13 @@
+//programa 1 estructura c++
+#include <stdio.h>
+#include <iostream>
+int main ()
+    \*
+    mi primer programa usando los lenguajes c y c++
+    realizado en el laboratorio G-202
+    authot rafael yepez
+    */
+   print f("hola mundo")
+   std::cout<<"estamos en c++"<<std::end;
+   std::cout<"c++ esta bonito";
+   return 0;
