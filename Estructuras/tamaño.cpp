69777903939e58d0400b6193eea134f9c436acd7
rafael yepez <xander21yl@gmail.com>
2024-11-20 10:05:53 -0500

    Tamaño 2

--- Estructuras/tamaño.cpp
+++ Estructuras/tamaño.cpp
@@ -1,7 +1,10 @@
 #include <stdio.h>
 main(){
     printf("El tamaño del char es %d : bytes\n", sizeof(char));
-    printf("El tamaño de un bool es %d : bytes\n", sizeof(bool))
-    printf("El tamaño de short int es %d : bytes\n", sizeof(short in))
-    printf()
+    printf("El tamaño de un bool es %d : bytes\n", sizeof(bool));
+    printf("El tamaño de short int es %d : bytes\n", sizeof(short in));
+    printf("El tamaño de long int es %d : bytes\n", sizeof(long int));
+    printf("El tamaño de un float es %d : bytes\n", sizeof(float));
+    printf("El tamaño de un Dloble es %d : bytes\n", sizeof(double));
+    printf("El tamaño de long double es %d : bytes\n", sizeof(long double));
 }
