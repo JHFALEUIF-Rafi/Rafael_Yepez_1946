63c85a7c5b144513ef351dd1605d48b99e028c6f
rafael yepez <xander21yl@gmail.com>
2024-11-20 09:57:04 -0500

    Tamaño bytes 1

--- Estructuras/ejercicio1.cpp
+++ Estructuras/ejercicio1.cpp
@@ -1,8 +1,7 @@
 //programa 1 estructura c++
 #include <stdio.h>
 #include <iostream>
-int main ()
-    \*
+int main (){\*
     mi primer programa usando los lenguajes c y c++
     realizado en el laboratorio G-202
     authot rafael yepez
@@ -12,3 +11,5 @@ int main ()
    std::cout<<"estamos en c++"<<std::end;
    std::cout<"c++ esta bonito";
    return 0;
+   }
+    
---
+++ Estructuras/tamaño.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>
+main(){
+    printf("El tamaño del char es %d : bytes\n", sizeof(char));
+    printf("El tamaño de un bool es %d : bytes\n", sizeof(bool))
+    printf("El tamaño de short int es %d : bytes\n", sizeof(short in))
+    printf()
+}
