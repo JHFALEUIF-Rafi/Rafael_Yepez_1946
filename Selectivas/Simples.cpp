82258bc21f84941a7262f02365855a18209a7e0d
rafael yepez <xander21yl@gmail.com>
2024-11-22 10:12:29 -0500

    Estructuras selectivas simples 1

---
+++ Selectivas/Simples.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+main(){
+    int num, r;
+    cout << "Ingrese un número: ";
+    cin >> num;
+    r = num %3;
+    if (r==0);
+    {
+        cout << "El número es múltiplo de tres"
+    }
+    num = 0;
+}
