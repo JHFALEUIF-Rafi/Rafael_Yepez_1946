7ada667bfdbc441907f727752671db7497064979
rafael yepez <xander21yl@gmail.com>
2024-11-22 10:48:18 -0500

    Estructuras selectivas simples 3

--- Selectivas/Simples.cpp
+++ Selectivas/Simples.cpp
@@ -1,13 +1,21 @@
 #include <iostream>
+#include <stdio.h>
 using namespace std;
-main(){
-    int num, r;
+main()
+{
+    int num, r, div;
     cout << "Ingrese un número: ";
     cin >> num;
-    r = num %3;
+    cout << "Ingrese número divisor"
+    cin >> div;
+    r = num % 3;
     if (r==0);
     {
-        cout << "El número es múltiplo de tres"
+        cout << "El número" << num <<"es divisible por " << div << endl;
+    }
+    else 
+    {
+        cout << "El número no es divisible para 3 "
     }
     num = 0;
 }
