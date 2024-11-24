96ed137e83ce0c7253159f1aa546bca8828d47a9
rafael yepez <xander21yl@gmail.com>
2024-11-23 21:48:31 -0500

    simples v2

--- Selectivas/Simples.cpp
+++ Selectivas/Simples.cpp
@@ -1,21 +1,22 @@
 #include <iostream>
 #include <stdio.h>
 using namespace std;
+/* Rafael Yépez*/
 main()
 {
     int num, r, div;
     cout << "Ingrese un número: ";
     cin >> num;
-    cout << "Ingrese número divisor"
+    cout << "Ingrese número divisor";
     cin >> div;
-    r = num % 3;
-    if (r==0);
+    r = num % div;
+    if (r==0)
     {
         cout << "El número" << num <<"es divisible por " << div << endl;
     }
     else 
     {
-        cout << "El número no es divisible para 3 "
+        cout << "El número" << num <<" no es divisible por " << div << endl;
     }
     num = 0;
 }
