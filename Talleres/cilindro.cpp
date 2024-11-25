fbb153809f504fa9dd6356b2f841ae7e083d471d
rafael yepez <xander21yl@gmail.com>
2024-11-24 20:49:04 -0500

    Cilindro v1

--- Talleres/cilindro.cpp
+++ Talleres/cilindro.cpp
@@ -1,1 +1,29 @@
 #include <iostream>
+#include <numbers>
+/*
+Integrantes: 
+-Alexis Chicaiza
+-Brandon Collahuazo
+-Jhoel Pallaroso
+-Rafael Yépez
+*/
+using namespace std;
+int main(){
+    float r, h, volumen, area;
+    cout << "Ingrese radio: ";
+    cin >> r;
+    cout << "Ingrese altura: ";
+    cin >> h;
+
+    volumen = 3.14 *( r * r) * h; 
+    area = 2 * 3.14 * r * (h + r);
+    if (h > r)
+    {
+       cout << "El volumen del cilindro es: " << volumen << endl; 
+    }
+    else
+    {
+        cout << "El area del cilindro es: " << area << endl;
+    }   
+return 0;
+}
--- Talleres/electricidad.cpp
+++ Talleres/electricidad.cpp
@@ -1,22 +1,28 @@
 #include <iostream>
-#include <stdio.h>
 using namespace std;
+/*
+Integrantes: 
+-Alexis Chicaiza
+-Brandon Collahuazo
+-Jhoel Pallaroso
+-Rafael Yépez
+*/
 int main(){
-    float kw, monto, incremento, total;
-    cout << "Ingrese consumo de electricidad en kw: ";
-    cin >> kw;
+    float kwh, monto, incremento, total;
+    cout << "Ingrese consumo de electricidad en kwh: ";
+    cin >> kwh;
     
-    monto = ( kw * 0.10 );
+    monto = ( kwh * 0.10 );
     incremento = (monto * 0.5);
 
-    if (kw > 700)
+    if (kwh > 700)
     {
         total = incremento + monto;
-        cout << "Valor a pagar: " << total << " dolares";
+        cout << "Valor a pagar: " << total << " dólares" << endl;
     }
     else
     {
-        cout << "El valor a pagar es: " << monto << " dolares" << endl;
+        cout << "El valor a pagar es: " << monto << " dólares" << endl;
     }
     return 0; 
 }
