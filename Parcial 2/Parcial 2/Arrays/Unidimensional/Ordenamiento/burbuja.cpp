f31be0d05135e318c44f4ebb718565e45f90aed5
rafael yepez <xander21yl@gmail.com>
2025-01-24 10:58:42 -0500

    Viernes 24 enero

--- Parcial 2/Parcial 2/Arrays/Unidimensional/Ordenamiento/burbuja.cpp
+++ Parcial 2/Parcial 2/Arrays/Unidimensional/Ordenamiento/burbuja.cpp
@@ -11,7 +11,6 @@ void llenarTabla(int v[], int n)
         v[i] = rand() % 20 + 1;
     }
 }
-
 int ordenarBurbuja(int v[], int n)
 {
     int aux, contador = 0;
@@ -31,11 +30,70 @@ int ordenarBurbuja(int v[], int n)
     return contador;
 }
 
+int ordenarBurbujaV2(int v[], int n)
+{
+    int mejora = 1;
+    int aux, contador = 0;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n - mejora; j++)
+        {
+            contador++;
+            if (v[j] > v[j + 1])
+            {
+                aux = v[j];
+                v[j] = v[j + 1];
+                v[j + 1] = aux;
+            }
+        }
+        mejora++;
+    }
+    return contador;
+}
+
+int ordenarBurbujaV3(int v[], int n)
+{
+    int mejora = 1;
+    bool change = true;
+    int aux, contador = 0;
+    for (int i = 0; i < n && change; i++)
+    {
+        change = false;
+        for (int j = 0; j < n - mejora; j++)
+        {
+            contador++;
+            if (v[j] > v[j + 1])
+            {
+                aux = v[j];
+                v[j] = v[j + 1];
+                v[j + 1] = aux;
+                change = true;
+            }
+        }
+        mejora++;
+    }
+    return contador;
+}
+
 void mostrarVector(int v[], int n)
 {
     for (int i = 0; i < n; i++)
     {
         cout << v[i] << " ";
+    }
+}
+
+void comparaciones(int mejor, int peor)
+{   
+    if (mejor < peor)
+    {
+        cout << "\nEl numero de comparaciones de " << mejor << " es mejor que " << peor << endl;
+    }
+    else if(mejor>peor){
+        cout << "\nEl numero de comparaciones de " << peor << " es mejor que " << mejor << endl;
+    }
+    else{
+        cout<<"\nEl numero de comparaciones son iguales"<<endl;
     }
 }
 
@@ -46,11 +104,29 @@ int main()
     cin >> ne;
     int vec[ne];
     llenarTabla(vec, ne);
-    cout << "Vector original: ";
+    cout << "\nVector original: ";
     mostrarVector(vec, ne);
     cout << endl;
-    cout<< "El numero de comparaciones burbuja v1 es: "<<ordenarBurbuja(vec, ne)<<endl;
+    cout << "El numero de comparaciones burbuja v1 es: " << ordenarBurbuja(vec, ne) << endl;
+    cout << "Vector ordenado" << endl;
+    
+    cout << "\nVector original: ";
+    mostrarVector(vec, ne);
+    cout << endl;
+    cout << "El numero de comparaciones burbuja v2 es: " << ordenarBurbujaV2(vec, ne) << endl;
+    cout << "Vector ordenado" << endl;
+    cout << "Vector ordenado" << endl;
+
+    cout << "\nVector original: ";
+    mostrarVector(vec, ne);
+    cout << endl;
+    cout << "El numero de comparaciones burbuja v3 es: " << ordenarBurbujaV3(vec, ne) << endl;
     cout << "Vector ordenado" << endl;
     mostrarVector(vec, ne);
+
+    int mejor = ordenarBurbuja(vec,ne);
+    int peor = ordenarBurbujaV3(vec, ne);
+    
+    comparaciones(mejor, peor);
     return 0;
 }
